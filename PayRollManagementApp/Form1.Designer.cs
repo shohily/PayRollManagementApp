3d94965f0fe2288aac2e91f8d9037a6e3284d5fd
shohily <shohely.haque@gmail.com>
2015-06-03 09:37:39 -0700

    Pay roll management

---
+++ PayRollManagementApp/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace PayRollManagementApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Basic Salary";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(124, 29);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(126, 23);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(124, 81);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(126, 23);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(448, 120);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(146, 23);
+            this.searchTextBox.TabIndex = 6;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Font = new System.Drawing.Font("Segoe Marker", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertButton.Location = new System.Drawing.Point(286, 52);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(117, 36);
+            this.insertButton.TabIndex = 3;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Font = new System.Drawing.Font("Segoe Marker", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(428, 52);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(136, 36);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter Employee ID";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Segoe Marker", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(581, 52);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(126, 36);
+            this.searchButton.TabIndex = 7;
+            this.searchButton.Text = "Search Info";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(124, 123);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(126, 23);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView.Location = new System.Drawing.Point(15, 171);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(694, 199);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Employee ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Employee Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Basic Salary";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "House Rent";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Medical Allowance";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Tax";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Total Salary";
+            this.Column7.Name = "Column7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 385);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Payroll Management System";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ PayRollManagementApp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayRollManagementApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string connectionstring = @"server =USER;database=DB_PayRollSystem;integrated security=true";
+        private int employeeID;
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            employeeID = Convert.ToInt16(idTextBox.Text);
+            string employeeName = nameTextBox.Text;
+            double basicSalary = Convert.ToDouble(salaryTextBox.Text);
+            double houseRent = (Convert.ToDouble(basicSalary))*20/100;
+            double Medical = (Convert.ToDouble(basicSalary))*15/100;
+            double Tax = (Convert.ToDouble(basicSalary))*5/100;
+            double totalSalary = Convert.ToDouble(basicSalary) + houseRent + Medical - Tax;
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string query =
+                string.Format(@"insert into PayRoll_Table(EmployeeID,EmployeeName,BasicSalary,HouseRent,MedicalAllowance,Tax,TotalSalary)
+                                 values('{0}','{1}','{2}','{3}','{4}','{5}','{6}')", employeeID, employeeName,
+                    basicSalary, houseRent, Medical, Tax, totalSalary);
+            SqlCommand aCommand = new SqlCommand(query, aConnection);
+            MessageBox.Show("Data has been inserted");
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            salaryTextBox.Text = "";
+            aCommand.ExecuteNonQuery();
+            aConnection.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+            string tableSql = "SELECT * FROM PayRoll_Table";
+            SqlCommand selectAllTableSqlCommand = new SqlCommand(tableSql, aConnection);
+            SqlDataReader tableReader = selectAllTableSqlCommand.ExecuteReader();
+
+            if (tableReader != null)
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                        tableReader[2].ToString(), tableReader[3].ToString(),
+                        tableReader[4].ToString(), tableReader[5].ToString(), tableReader[6].ToString());
+                }
+            tableReader.Close();
+            aConnection.Close();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+            employeeID = Convert.ToInt16(searchTextBox.Text);
+
+
+            SqlCommand searchByEmployeeID =
+                new SqlCommand("SELECT * FROM PayRoll_Table WHERE EmployeeID like '" + employeeID + "'", aConnection);
+
+            SqlDataReader tableReader = searchByEmployeeID.ExecuteReader();
+            if (tableReader != null)
+            {
+                bool check = true;
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                        tableReader[2].ToString(), tableReader[3].ToString(),
+                        tableReader[4].ToString(), tableReader[5].ToString(), tableReader[6].ToString());
+                    check = false;
+                }
+                if (check)
+                {
+                    MessageBox.Show("Your desired employee ID information is not present in the database. ");
+                }
+                tableReader.Close();
+                aConnection.Close();
+            }
+
+
+        }
+    }
+}
